8716f21fdbe2ea9a9c456a24cae9b4c7de28348a
Sean Pike <sean@dhcp-197-171.caltech.edu>
2018-04-23 16:14:02 -0700

    spectral fitting

--- noise/noise.py
+++ noise/noise.py
@@ -14,8 +14,8 @@ mask = data['TEMP'] > -20
 START = np.argmax(mask)
 END = len(mask) - np.argmax(mask[::-1])
 
-trigX = [3*j for j in range(5)] + [3*j + 16 for j in range(6)]
-trigY = [3*j for j in range(5)] + [3*j + 16 for j in range(6)]
+trigX = [(3*j) + 1  for j in range(11)]
+trigY = [(3*j) + 1  for j in range(11)]
 
 channel = []
 channelMap = [[[] for i in range(32)] for j in range(32)]
@@ -36,8 +36,7 @@ plt.figure()
 plt.plot(range(len(spectrum[0])), spectrum[0])
 plt.show()
 plt.close()'''
-print(rawx)
-print(rawy)
+
 pixelmap = np.histogram2d(rawx, rawy, bins = (32,32))
 plt.figure()
 plt.imshow(pixelmap[0])
@@ -53,12 +52,12 @@ for x in trigX:
 			trigSum += 1
 			#print(x)
 			#print(y)
-			#tempSpec = np.histogram(channelMap[x][y], bins=int(np.ceil(np.max(channelMap[x][y]))))
-			#centroid = np.argmax(tempSpec[0])
-			#fit_channels = np.arange(centroid-100, centroid + 250)
-			#g_init = models.Gaussian1D(amplitude=tempSpec[0][centroid], mean=centroid, stddev = 75)
-			#fit_g = fitting.LevMarLSQFitter()
-			#g = fit_g(g_init, range(len(tempSpec[0])), tempSpec[0])
+			tempSpec = np.histogram(channelMap[x][y], bins=int(np.ceil(np.max(channelMap[x][y]))))
+			centroid = np.argmax(tempSpec[0])
+			fit_channels = np.arange(centroid-100, centroid + 250)
+			g_init = models.Gaussian1D(amplitude=tempSpec[0][centroid], mean=centroid, stddev = 75)
+			fit_g = fitting.LevMarLSQFitter()
+			g = fit_g(g_init, range(len(tempSpec[0])), tempSpec[0])
 			#plt.plot(range(len(tempSpec[0])), tempSpec[0])
 			#plt.plot(range(len(tempSpec[0])), g(range(len(tempSpec[0]))))
 			#plt.show()
