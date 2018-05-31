bf0e01ee63bacf33f134e89e589e3a9fed4def07
Sean Pike <spike@caltech.edu>
2018-05-31 16:26:06 -0700

    bug fixes

--- gammaFlood/quickGain.py
+++ gammaFlood/quickGain.py
@@ -91,7 +91,7 @@ empty = np.transpose(np.nonzero(gain == 0.0))
 #print(empty)
 for x in empty:
 	temp = newgain[x[0]:x[0]+3, x[1]:x[1]+3]
-	gain[x] = np.sum(temp)/np.count_nonzero(temp)
+	gain[x[0], x[1]] = np.sum(temp)/np.count_nonzero(temp)
 
 plt.figure()
 plt.imshow(gain)
