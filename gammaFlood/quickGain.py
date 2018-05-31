c09822efad9a8a764d793144f95c4c70c5db317e
Sean Pike <spike@caltech.edu>
2018-05-30 23:52:26 -0700

    bug fixes

--- gammaFlood/quickGain.py
+++ gammaFlood/quickGain.py
@@ -46,7 +46,7 @@ for x in range(32):
 
 		if len(channel):
 			spectrum = np.histogram(channel, bins=bins, range = (0, maxchannel))
-			centroid = np.argmax(spectrum[0][1000:]) + 1000
+			centroid = np.argmax(spectrum[0][3000:]) + 3000
 			fit_channels = np.arange(centroid-100, centroid + 200)
 			g_init = models.Gaussian1D(amplitude=spectrum[0][centroid], mean=centroid, stddev = 75)
 			fit_g = fitting.LevMarLSQFitter()
