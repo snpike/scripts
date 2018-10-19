c14abf848076189524502a16796decb07a878b27
Sean Pike <spike@caltech.edu>
2018-10-18 19:48:18 -0700

    plotting and full gain correction

--- gammaFlood/gamma_small.py
+++ gammaFlood/gamma_small.py
@@ -8,6 +8,7 @@ import os.path
 import pickle
 import seaborn as sns
 from matplotlib.gridspec import GridSpec
+import scipy.stats as stats
 
 sns.set_context('talk')
 sns.set_style("ticks")
@@ -49,8 +50,114 @@ else:
 gainBool = os.path.exists(gainpath)
 
 maxchannel = 10000
+bins = 10000
 
 spectra = {}
+
+buff_gain = np.zeros((34, 34))
+
+if gainBool:
+    buff_gain[1:33][1:33] = numpy.loadtxt(gainpath)
+
+else:
+    # Collect energy-channel pairs at each spectral line which we will later fit to a polynomial to find the gain
+    gain_points = [[[] for x in range(32)] for y in range(32)]
+
+    gain = np.zeros((32, 32))
+
+    for source in sourcelist:
+        lines = line_dict[source]
+        latex_label = latex_dict[source]
+        filepath = filepath_dict[source]
+
+        slash = 0
+        i = 0
+        for char in filepath:
+            if char == '/':
+                slash = i
+            i += 1
+
+        filename = filepath[slash + 1:]
+
+        file = fits.open(filepath)
+        data = file[1].data
+        file.close()
+
+        T_mask = data['TEMP'] > -20
+
+        START = np.argmax(T_mask)
+        END = len(T_mask) - np.argmax(T_mask[::-1])
+
+        STIMmask = np.array(data['STIM'][START:END])==0
+
+        for row in np.arange(region[0][1], region[1][1] + 1):
+
+            row_mask = data['RAWY'] == row
+            
+            for col in np.arange(region[0][0], region[1][0] + 1):
+                plt.figure()
+
+                col_mask = data['RAWX'] == col
+                channel = data.field('PH')[START:END][np.nonzero(np.multiply(np.multiply((col_mask), (row_mask)), data.field('GRADE')[START:END] == 0))]
+            
+                if len(channel):
+
+                    spectrum = plt.hist(channel, bins=bins, range = (0, maxchannel))
+                    
+                    for line in lines:
+                    
+                        centroid = np.argmax(spectrum[0][int(line/0.013)-500:int(line/0.013)+500]) + int(line/0.013)-500
+                        fit_channels = np.arange(centroid-70, centroid + 150)
+                        g_init = models.Gaussian1D(amplitude=spectrum[0][centroid], mean=centroid, stddev = 75)
+                        fit_g = fitting.LevMarLSQFitter()
+                        g = fit_g(g_init, fit_channels, spectrum[0][fit_channels])
+                
+                        if fit_g.fit_info['param_cov'] is not None:
+                            
+                            plt.plot(fit_channels, g(fit_channels), label = 'Gaussian fit (' + str(round(line, 0)) + ' keV)')
+                            plt.ylabel('Counts')
+                            plt.xlabel('Channel')
+                            plt.legend()
+                            plt.savefig('/users/spike/det_figs/' + detector + '/pixels/' + filename[:-5] + '_x' + str(x) + '_y' + str(y) + '_spec.pdf')
+
+                            # If the gain is not near  0.013 then the spectrum was probably not good enough to get a real gain value. Skip it
+                            if 0.01 < (line/g.mean) < 0.016:
+                                # Put the line energy (in keV) and the mean in channels in the gain data points
+                                gain_points[row][col].append([g.mean, line])
+
+    for row in range(32):
+        for col in range(32):
+            if len(gain_points[row][col]):
+                points = np.array(gain_points[row][col]).T
+                gain[row][col] = stats.linregress(points[0], y = points[1])[0]
+
+    # interpolate gain for pixels where fit was unsuccessful
+    newgain = np.zeros((34,34))
+    newgain[1:33, 1:33] = gain
+    empty = np.transpose(np.nonzero(gain == 0.0))
+    #print(empty)
+    for x in empty:
+        if region[0][1] < x[0] < region[1][1]:
+            if region[0][0] < x[1] < region[1][0]:
+                temp = newgain[x[0]:x[0]+3, x[1]:x[1]+3]
+                if np.count_nonzero(temp):
+                    gain[x[0], x[1]] = np.sum(temp)/np.count_nonzero(temp)
+
+    # One more pass for good measure
+    newgain = np.zeros((34,34))
+    newgain[1:33, 1:33] = gain
+    empty = np.transpose(np.nonzero(gain == 0.0))
+    #print(empty)
+    for x in empty:
+        if region[0][1] < x[0] < region[1][1]:
+            if region[0][0] < x[1] < region[1][0]:
+                temp = newgain[x[0]:x[0]+3, x[1]:x[1]+3]
+                if np.count_nonzero(temp):
+                    gain[x[0], x[1]] = np.sum(temp)/np.count_nonzero(temp)
+
+    np.savetxt('/disk/lif2/spike/detectorData/' + detector + '/fullgain_region_low_x' + str(region[0][0]) + '_y' + str(region[0][1]) + 'high_x' + str(region[1][0]) + '_y' + str(region[1][1]) + '.npy', gain)
+    buff_gain[1:33][1:33] = gain
+
 
 for source in sourcelist:
 
@@ -80,7 +187,6 @@ for source in sourcelist:
 
     countMap = [[np.sum(np.multiply(STIMmask, np.multiply(np.array(data['RAWX'][START:END])==col, np.array(data['RAWY'][START:END])==row))) for col in np.arange(region[0][0], region[1][0] + 1)] for row in np.arange(region[0][1], region[1][1] + 1)]
 
-    pix_on = np.nonzero(countMap)
     count_array = np.array(countMap)
 
     # Plot some info about counts
@@ -91,8 +197,8 @@ for source in sourcelist:
     plt.imshow(masked)
     c = plt.colorbar()
     c.set_label('Counts')
-    plt.xticks(np.arange(0,region[1][0]-region[0][0]+1), np.arange(region[0][0], region[1][0] + 1))
-    plt.yticks(np.arange(0,region[1][1]-region[0][1]+1), np.arange(region[0][1], region[1][1] + 1))
+    plt.xticks(np.arange(0,region[1][0]-region[0][0]+1), [str(int(x)) for x in np.arange(region[0][0], region[1][0] + 1)])
+    plt.yticks(np.arange(0,region[1][1]-region[0][1]+1), [str(int(x)) for x in np.arange(region[0][1], region[1][1] + 1)])
     plt.tight_layout()
     plt.savefig('/users/spike/det_figs/' + detector + '/' + filename[:-5] + '_floodmap.pdf')
     plt.close()
@@ -106,49 +212,32 @@ for source in sourcelist:
     plt.close()
 
     # If there's gain data then correct the spectrum
-    gain = np.zeros((34, 34))
     energyList = []
 
-    if gainBool:
-        gain[1:33,1:33] = pickle.load(open(gainpath, 'rb'))
-        # for event in data[START:END]:
-        #     row = event['RAWY']
-        #     col = event['RAWX']
-        #     if [row, col] not in badpix:
-        #         temp = event['PH_COM'].reshape(3,3)
-        #         mask = (temp > 0).astype(int)
-        #         energyList.append(np.sum(np.multiply(np.multiply(mask, temp), gain[row:row + 3, col:col + 3])))
-
-    else:
-        for row in np.arange(region[0][1], region[1][1] + 1):
-            row_mask = data['RAWY'] == row
-            for col in np.arange(region[0][0], region[1][0] + 1):
-                if [row, col] not in badpix:
-                    col_mask = data['RAWX'] == col
 
     for row in np.arange(region[0][1], region[1][1] + 1):
         row_mask = data['RAWY'] == row
         for col in np.arange(region[0][0], region[1][0] + 1):
-            if [row, col] not in badpix:
-                col_mask = data['RAWX'] == col
-                # Getting indices ('inds') and PH_COM values ('pulses') of 
-                # all events at current pixel.
-                inds = np.nonzero(np.multiply(np.multiply(row_mask, col_mask), T_mask))
-                pulses = data.field('PH_COM')[inds]
-                # The gain for the 3x3 grid around this pixel
-                gain_grid = gain[row:row + 3, col:col + 3]
-                # iterating through the PH_COM values for this pixel
-                for pulse in pulses:
-                    # Append the sum of positive energies in the 
-                    # pulse grid to 'energies'
-                    pulse_grid = pulse.reshape(3, 3)
-                    mask = (pulse_grid > 0).astype(int)
-                    energyList.append(np.sum(np.multiply(
-                        np.multiply(mask, pulse_grid), gain_grid)))
+            col_mask = data['RAWX'] == col
+            # Getting indices ('inds') and PH_COM values ('pulses') of 
+            # all events at current pixel.
+            inds = np.nonzero(np.multiply(np.multiply(row_mask, col_mask), T_mask))
+            pulses = data.field('PH_COM')[inds]
+            # The gain for the 3x3 grid around this pixel
+            gain_grid = buff_gain[row:row + 3, col:col + 3]
+            # iterating through the PH_COM values for this pixel
+            for pulse in pulses:
+                # Append the sum of positive energies in the 
+                # pulse grid to 'energies'
+                pulse_grid = pulse.reshape(3, 3)
+                mask = (pulse_grid > 0).astype(int)
+                energyList.append(np.sum(np.multiply(np.multiply(mask, pulse_grid), gain_grid)))
 
 
     bins = 10000
     spectra[source] = np.histogram(energyList, bins = bins, range= (0.01, 120))
+    plt.figure()
+    plt.plot(spectrum[1][:-1], spectrum[0], label = latex_label)
 
     for line in lines:
         centroid = np.argmax(spectrum[0][int(line/0.013)-500:int(line/0.013)+500]) + int(line/0.013)-500
@@ -156,58 +245,46 @@ for source in sourcelist:
         g_init = models.Gaussian1D(amplitude=spectrum[0][centroid], mean=centroid, stddev = 75)
         fit_g = fitting.LevMarLSQFitter()
         g = fit_g(g_init, fit_channels, spectrum[0][fit_channels])
-        # print(np.diag(fit_g.fit_info['param_cov']))
         sigma_err = np.diag(fit_g.fit_info['param_cov'])[2]
         fwhm_err = 2*np.sqrt(2*np.log(2))*sigma_err
         mean_err = np.diag(fit_g.fit_info['param_cov'])[1]
         frac_err = np.sqrt(np.square(fwhm_err) + np.square(g.fwhm*mean_err/g.mean))/g.mean
-        # print(g.fwhm/g.mean)
-        # print(frac_err)
-        # print(Am_line * g.fwhm/g.mean)
-        # print(frac_err * Am_line)
-        plt.text(70, spectrum[0][centroid]*3/5, r'$\mathrm{FWHM}=$' + str(int(round(Am_line * 1000 * g.fwhm/g.mean, 0))) + r'$\pm$' + str(int(round(frac_err * Am_line*1000))) + ' eV', fontsize=13)
-
-        plt.plot(spectrum[1][:-1], spectrum[0], label = r'${}^{241}{\rm Am}$')
         plt.plot(spectrum[1][fit_channels], g(fit_channels), label = 'Gaussian fit')
-        plt.xlabel('Energy (keV)')
-        plt.ylabel('Counts')
-        plt.legend()
+        print('FWHM ' + str(g.fwhm) + '+/-' + str(fwhm_err) + ' at ' str(round(line, 0)) + ' keV')
 
-        plt.tight_layout()
-        plt.savefig('/users/spike/det_figs/' + detector + '/' + filename[:-5] + '_gammaspec_gain.pdf')
-        plt.close()
+    plt.xlabel('Energy (keV)')
+    plt.ylabel('Counts')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('/users/spike/det_figs/' + detector + '/' + filename[:-5] + '_spec_gain.pdf')
+    plt.close()
 
-    else:
-        bins = np.arange(1,maxchannel)
-        spectrum = np.histogram(data['PH'][START:END], bins = bins, range= (0, maxchannel))
+plt.figure()
 
-        centroid = np.argmax(spectrum[0][3000:]) + 3000
-        fit_channels = np.arange(centroid-100, centroid + 200)
+for source in sourcelist:
+    lines = line_dict[source]
+    latex_label = latex_dict[source]
+    spectrum = spectra[source]
+
+    plt.plot(spectrum[1][:-1], spectrum[0], label = latex_label)
+
+    for line in lines:
+        centroid = np.argmax(spectrum[0][int(line/0.013)-500:int(line/0.013)+500]) + int(line/0.013)-500
+        fit_channels = np.arange(centroid-70, centroid + 150)
         g_init = models.Gaussian1D(amplitude=spectrum[0][centroid], mean=centroid, stddev = 75)
         fit_g = fitting.LevMarLSQFitter()
         g = fit_g(g_init, fit_channels, spectrum[0][fit_channels])
-        print(np.diag(fit_g.fit_info['param_cov']))
         sigma_err = np.diag(fit_g.fit_info['param_cov'])[2]
         fwhm_err = 2*np.sqrt(2*np.log(2))*sigma_err
         mean_err = np.diag(fit_g.fit_info['param_cov'])[1]
         frac_err = np.sqrt(np.square(fwhm_err) + np.square(g.fwhm*mean_err/g.mean))/g.mean
-        print(g.fwhm/g.mean)
-        print(frac_err)
-        print(Am_line * g.fwhm/g.mean)
-        print(frac_err * Am_line)
-        #plt.text(maxchannel*3/5, spectrum[0][centroid]*3/5, r'$\mathrm{FWHM}=$' + str(int(g.fwhm)) + r'$\pm$' + str(int(2*np.sqrt(2*np.log(2))*sigma_err)), fontsize=16)
-        #plt.text(maxchannel*3/5, spectrum[0][centroid]*3/5, r'$\mathrm{\frac{FWHM}{\mu}}=$' + str(int(round(100*g.fwhm/g.mean, 0))) + '%', fontsize=14)
-        plt.text(maxchannel*3/5, spectrum[0][centroid]*3/5, r'$\mathrm{FWHM}=$' + str(int(round(Am_line * 1000 * g.fwhm/g.mean, 0))) + r'$\pm$' + str(int(round(frac_err * Am_line*1000))) + ' eV', fontsize=13)
+        plt.plot(spectrum[1][fit_channels], g(fit_channels), label = 'Gaussian fit')
 
-        plt.plot(spectrum[1][:-1], spectrum[0], label = r'${}^{241}{\rm Am}$')
-        plt.plot(fit_channels, g(fit_channels), label = 'Gaussian fit')
-        plt.xlabel('Channel')
-        plt.ylabel('Counts')
-        plt.legend()
+plt.xlabel('Energy (keV)')
+plt.ylabel('Counts')
+plt.legend()
+plt.tight_layout()
+plt.savefig('/users/spike/det_figs/' + detector + '/fullspec_region_low_x' + str(region[0][0]) + '_y' + str(region[0][1]) + 'high_x' + str(region[1][0]) + '_y' + str(region[1][1]) + '.pdf')
+plt.close()
 
-        #plt.title(detector + ' ' + source + ' Spectrum ' + '(' + etc + ')')
-        plt.tight_layout()
-        #plt.savefig('/disk/lif2/spike/detectorData/' + detector + '/figures/' + filename[:-4] + 'gammaspec.pdf')
-        plt.savefig('/users/spike/det_figs/' + detector + '/' + filename[:-5] + '_gammaspec.pdf')
-        #plt.show()
-        plt.close()
+
