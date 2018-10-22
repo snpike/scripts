ea88e633a77d05143488ab96559b07e19cb0d7cf
Sean Pike <spike@caltech.edu>
2018-10-22 14:49:00 -0700

    new script for taking npy files to fits

---
+++ noise/offset_npy2fits.py
@@ -0,0 +1,42 @@
+from astropy.io import fits
+import numpy as np
+import os.path
+
+filepath = input('Enter the path to the .npy offset file to convert:').strip()
+npy_data = np.loadtxt(filepath)
+
+while npy_data.shape != (32,32,16):
+	print('The shape of the offset array should be (32, 32, 16). Instead got ' + str(offsets.shape) + '. Try again.')
+	filepath = input('Enter the path to the .npy offset file to convert:').strip()
+	npy_data = np.loadtxt(filepath)
+
+slash = 0
+i = 0
+for char in filepath:
+    if char == '/':
+        slash = i
+    i += 1
+
+filename = filepath[slash + 1:]
+
+detector = input('Please enter the detector ID: ').strip()
+
+rawx = []
+rawy = []
+offset = []
+
+for row in range(32):
+	for col in range(32)
+		rawx.append(col)
+		rawy.append(row)
+		offset.append(npy_data[row][col])
+
+col1 = fits.Column(name = 'RAWX', format = '1B', array = rawx)
+col2 = fits.Column(name = 'RAWY', format = '1B', array = rawy)
+col3 = fits.Column(name = 'OFFSET', format = '16E', array = offset)
+
+coldefs = fits.ColDefs([col1, col2, col3])
+
+hdu = fits.BinTableHDU.from_columns(coldefs)
+
+hdu.writeto('/disk/lif2/spike/detectorData/' + str(detector) + '/' + filename[:-4] + '.fits')
