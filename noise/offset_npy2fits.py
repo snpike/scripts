a0caed9166bd5203e8a33cf5b32c2a7b1b793a14
Sean Pike <spike@caltech.edu>
2018-10-22 16:29:17 -0700

    bug fixes

--- noise/offset_npy2fits.py
+++ noise/offset_npy2fits.py
@@ -38,5 +38,12 @@ col3 = fits.Column(name = 'OFFSET', format = '16E', array = offset)
 coldefs = fits.ColDefs([col1, col2, col3])
 
 hdu = fits.BinTableHDU.from_columns(coldefs)
+hdu1 = fits.BinTableHDU.from_columns(coldefs)
+hdu2 = fits.BinTableHDU.from_columns(coldefs)
+hdu3 = fits.BinTableHDU.from_columns(coldefs)
+
+hdu.append(hdu1)
+hdu.append(hdu2)
+hdu.append(hdu3)
 
 hdu.writeto(filepath[:-4] + '.fits')
