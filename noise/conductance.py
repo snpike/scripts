71e7cec0718411dc208a828695a6a61c6eab388b
Sean Pike <spike@caltech.edu>
2019-04-10 15:55:56 -0700

    Changing the color map

--- noise/conductance.py
+++ noise/conductance.py
@@ -4,7 +4,7 @@ import seaborn as sns
 
 sns.set_context('talk')
 sns.set_style('ticks')
-sns.set_palette('RdBu_r')
+sns.set_palette('colorblind')
 
 filepath = input('Please enter the path to the interpixel data: ').strip()
 pathsplit = filepath.split('/')
@@ -30,7 +30,7 @@ col_data = np.array(col_data).astype(int).T
 
 vmax = np.max([np.max(row_data), np.max(col_data)])
 plt.figure()
-plt.imshow(row_data, vmin=0, vmax=vmax)
+plt.imshow(row_data, vmin=0, vmax=vmax, cmap='coolwarm')
 c = plt.colorbar()
 c.set_label('(S/1.7e10)')
 plt.tight_layout()
@@ -38,7 +38,7 @@ plt.savefig('/users/spike/det_figs/' + detector + '/' + pathsplit[-3] + '_row_ma
 plt.close()
 
 plt.figure()
-plt.imshow(col_data, vmin=0, vmax=vmax)
+plt.imshow(col_data, vmin=0, vmax=vmax, cmap='coolwarm')
 c = plt.colorbar()
 c.set_label('(S/1.7e10)')
 plt.tight_layout()
