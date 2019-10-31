febba4f23f6760da8b2b91193e9fd336ce6ca829
Sean Pike <spike@caltech.edu>
2019-10-30 17:16:27 -0700

    fix path to gti

--- J1739m285/nuproducts_epochs.sh
+++ J1739m285/nuproducts_epochs.sh
@@ -6,8 +6,8 @@ cd /disk/lif2/spike/J1739m285
 
 for x in "${epochs[@]}"
 do
-	nuproducts indir=./90501343002_pipe_out instrument=FPMA steminputs=nu90501343002 outdir=./products_epoch"$x" srcregionfile=./90501343002_pipe_out/srcA.reg bkgregionfile=./90501343002_pipe_out/bkgA.reg binsize=0.5 usrgtifile=epoch_"$x".curs_gti >& nuproductsA_epoch"$x".log
-	nuproducts indir=./90501343002_pipe_out instrument=FPMB steminputs=nu90501343002 outdir=./products_epoch"$x" srcregionfile=./90501343002_pipe_out/srcB.reg bkgregionfile=./90501343002_pipe_out/bkgB.reg binsize=0.5 usrgtifile=epoch_"$x".curs_gti >& nuproductsB_epoch"$x".log
+	nuproducts indir=./90501343002_pipe_out instrument=FPMA steminputs=nu90501343002 outdir=./products_epoch"$x" srcregionfile=./90501343002_pipe_out/srcA.reg bkgregionfile=./90501343002_pipe_out/bkgA.reg binsize=0.5 usrgtifile=./90501343002_pipe_out/epoch_"$x".curs_gti >& nuproductsA_epoch"$x".log
+	nuproducts indir=./90501343002_pipe_out instrument=FPMB steminputs=nu90501343002 outdir=./products_epoch"$x" srcregionfile=./90501343002_pipe_out/srcB.reg bkgregionfile=./90501343002_pipe_out/bkgB.reg binsize=0.5 usrgtifile=./90501343002_pipe_out/epoch_"$x".curs_gti >& nuproductsB_epoch"$x".log
 	cd products_epoch"$x"
 	grppha nu90501343002A01_sr.pha groupedA_25.pha comm="bad 0-35 & bad 1910-4095 & group min 25 & exit" >& grpphaA.log
 	grppha nu90501343002B01_sr.pha groupedB_25.pha comm="bad 0-35 & bad 1910-4095 & group min 25 & exit" >& grpphaB.log
